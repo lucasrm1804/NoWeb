65b04b4e9792585fe6b0442352aba7b81c2072cb
Lucas Monteiro <lucasribeirom@hotmail.com>
2023-08-14 17:58:57 -0300

    inicia a construção do header

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Headers from './components/Header/index.jsx'
+import './App.css'
+import React from 'react';
+
+function App(){
+  return (
+    <div className="frame1">
+      <Headers />
+    </div>
+  );
+}
+
+export default App
---
+++ my-app/src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import grupo13 from "../../imagens/Group 13.png";
+import frame from "../../imagens/Frame.png";
+
+
+function Header() {
+	return (
+    <header>
+      <div className="grupo40">
+
+        <div className="grupo14">
+          <img className="grupo13" src={grupo13}/>
+        <img className="frame" src={frame} />
+        </div>
+
+        <nav className="retangulo45">
+        oiashdfouih´
+        </nav>
+      </div>
+      
+    </header>
+	)
+}
+
+export default Header;
