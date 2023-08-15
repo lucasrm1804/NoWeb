9acdad65fd415e5f2a72bd2ee9ef3704a0e516de
Lucas Monteiro <lucasribeirom@hotmail.com>
2023-08-15 12:02:40 -0300

    cria e estiliza os chamados

--- my-app/src/components/Section1/index.jsx
+++ my-app/src/components/Section1/index.jsx
@@ -5,7 +5,13 @@ function Section1() {
 	return (
     <section className="section1">
       <div className="opac">
-        asfgfagsfdg
+        <div>
+          <h1>ILUMINE O SEU DIA A DIA!</h1>
+          <p>Aqui na Elétrica J. Santos queremos trazer mais luz para o seu dia a dia! Temos produtos para toda sua casa com a melhor qualidade e atendimento da região!</p>
+          <h2 className="chamada1">VEJA NOSSOS PRODUTOS</h2>
+          <h2 className="chamada2">NOS CONHEÇA MELHOR</h2>
+        </div>
+          
       </div>
     </section>
 	)
